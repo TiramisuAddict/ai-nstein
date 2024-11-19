e1704e9e51c154e2bc52b7a8d5efe4d5724e6714
Taha Medien <93660523+TiramisuAddict@users.noreply.github.com>
2024-11-19 21:20:13 +0100

    Expert SignIn

---
+++ Vue/addExpert.php
@@ -0,0 +1,23 @@
+<?php 
+    require_once '../Model/User.php';
+    require_once '../Controller/UserController.php';
+
+    $userController = new UserController();
+
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $experience = $_POST['experience'];
+
+    $proof = $_POST['proof'];
+
+    $user = new User($username, $email, $password, '', $age, $gender, '', $experience, '', $proof, "Expert",''); //if it works don't touch it
+
+    $userController->addUser($user);
+
+    header('Location: dashboard.php');
+
+    exit;
+?>
